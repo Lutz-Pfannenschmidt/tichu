dc2f7fc834fe987c99fd245e1a14a43016ff2c36
BuildTools <unconfigured@null.spigotmc.org>
2024-03-18 18:24:18 +0100

    dog test combination

--- internal/tichu/combination_test.go
+++ internal/tichu/combination_test.go
@@ -8,3 +8,7 @@ import (
 func TestPheonix(t *testing.T) {
 	fmt.Println(NewCombination([]*Card{&Card{Type: PHOENIX, Color: Special}}))
 }
+
+func TestDog(t *testing.T) {
+	fmt.Println(NewCombination([]*Card{&Card{Type: DOG, Color: Special}}))
+}
